b38feb6435255a81b88009566a19f5a0ebd318c6
Eugenia-Burakova <evzickabur@gmail.com>
2023-01-21 11:02:23 +0000

    additional solution

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ function printPrice() {
     let totalPrice = basicPrice + servicePrice + guaranteePrice;
     document.querySelector(".price-output").textContent = `${totalPrice} $`;
 }
+
+
+//ALSO WORKS
+// let basicPrice = 1000;
+// let servicePrice = 0;
+// let guaranteePrice = 0;
+
+// let services = document.querySelectorAll(".service");
+// services.forEach(chooseService);
+// function chooseService(service){
+//   service.addEventListener("click", (event) => {
+//     servicePrice = parseInt(service.dataset.service);
+//     printPrice();
+//     })
+// }
+
+// let guarantees = document.querySelectorAll(".guarantee");
+// guarantees.forEach(chooseGuarantee);
+// function chooseGuarantee(guarantee){
+//   guarantee.addEventListener("click", (event) => {
+//     guaranteePrice = parseInt(guarantee.dataset.guarantee);
+//     printPrice();
+//   })
+// }
+
+// function printPrice () {
+//   let totalPrice = basicPrice + servicePrice + guaranteePrice;
+//   document.querySelector(".price-output").textContent = `${totalPrice} $`;
+// }
